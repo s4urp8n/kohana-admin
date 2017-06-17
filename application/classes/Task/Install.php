9188c79e919fc25890424865355f731506bc4d2e
s4urp8n <s4urp8n@yandex.ru>
2017-06-18 02:06:01 +0300

    desktop version

--- application/classes/Task/Install.php
+++ application/classes/Task/Install.php
@@ -5,6 +5,15 @@ class Task_Install extends Minion_Task
 
     protected function _execute(array $params)
     {
+
+        chdir(DOCROOT);
+
+        passthru('git stash');
+        passthru('git stash clear');
+        passthru('git pull origin master');
+        passthru('composer update');
+        passthru('npm update');
+
         \Zver\Common::removeDirectoryContents(\Zver\FileCache::getDirectory());
 
         touch(\Zver\StringHelper::load(\Zver\FileCache::getDirectory())
@@ -14,6 +23,25 @@ class Task_Install extends Minion_Task
 
         $this->clearPreviewsAndUploads();
         $this->insertDump();
+
+        passthru('gulp');
+
+        $writable = [
+            DOCROOT . 'application' . DIRECTORY_SEPARATOR . 'cache',
+            DOCROOT . 'application' . DIRECTORY_SEPARATOR . 'logs',
+            DOCROOT . 'inc' . DIRECTORY_SEPARATOR . 'build',
+            DOCROOT . 'system' . DIRECTORY_SEPARATOR . 'uploads',
+            DOCROOT . 'system' . DIRECTORY_SEPARATOR . 'previews',
+        ];
+
+        foreach ($writable as $dir) {
+            chmod($dir, 0777);
+            foreach (\Zver\Common::getDirectoryContentRecursive($dir) as $path) {
+                if (is_dir($path)) {
+                    chmod($path, 0777);
+                }
+            }
+        }
     }
 
     protected function clearPreviewsAndUploads()
